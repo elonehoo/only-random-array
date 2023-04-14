5aff968fd1d6d9c0778cffa4c4975806139ce1aa
elonehoo <hi@elonehoo.me>
2023-04-14 10:19:16 +0800

    style: run eslint fix

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,16 +1,15 @@
-import { test,expect } from 'vitest'
+import { expect, test } from 'vitest'
 import onlyRandomArray from '../src'
 
-test('main',()=>{
-  const random = onlyRandomArray([1, 2, 3, 4]);
+test('main', () => {
+  const random = onlyRandomArray([1, 2, 3, 4])
 
-	let current;
-	let previous;
-	let index = 100;
-	while (index--) {
-		current = random();
+  let current
+  let previous
+  let index = 100
+  while (index--) {
+    current = random()
     expect(current).not.toBe(previous)
-		previous = current;
-	}
+    previous = current
+  }
 })
-
