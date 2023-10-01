eac18942144d27bfe2efae170ec482b23982ac48
elonehoo <hi@elonehoo.me>
2023-10-01 15:29:15 +0800

    chore: update dept

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import onlyRandom from '@elonehoo/only-random'
 
 export default function onlyRandomArray<T>(array: readonly T[]): () => T {
-  const random: Function = onlyRandom(0, array.length - 1)
+  const random = onlyRandom(0, array.length - 1)
   return () => array[random()]
 }
